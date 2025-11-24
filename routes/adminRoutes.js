069ac764a0b03736f9a991a556d7603d64fc04e7
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-24 13:48:34 +0700

    add endpoint untuk get statistic di routes admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,39 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-// IMPORT DUA CONTROLLER (PENTING!)
 const adminController = require('../controllers/adminController');
 const apikeyController = require('../controllers/apikeyController'); 
 
-// ==========================================
-// RUTE AUTH ADMIN (Login & Register)
-// ==========================================
 router.post('/register', adminController.register);
 router.post('/login', adminController.login);
 
-// ==========================================
-// RUTE DASHBOARD: USERS
-// ==========================================
 router.get('/users', adminController.getAllUsers);
-
-// ==========================================
-// RUTE DASHBOARD: API KEYS (CRUD LENGKAP)
-// ==========================================
-
-// 1. Ambil Semua Data (READ)
-// Pakai apikeyController, bukan adminController
 router.get('/apikeys', apikeyController.getAllApiKeys);
-
-// 2. Buat Key Manual (CREATE)
-// Method harus POST, bukan GET
 router.post('/apikeys', apikeyController.createApiKey);
-
-// 3. Update Expired Key (UPDATE)
-// Method harus PUT dan butuh parameter ID (:id)
 router.put('/apikeys/:id', apikeyController.updateApiKey);
-
-// 4. Hapus Key (DELETE)
-// Nama fungsi di controller adalah deleteKey
 router.delete('/apikeys/:id', apikeyController.deleteKey);
+router.get('/stats', adminController.getDashboardStats);
 
 module.exports = router;
